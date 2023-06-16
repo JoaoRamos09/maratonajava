576a78f495acc40e40276ce502d326f92ccc5231
João Vítor Ramos <136603790+JoaoRamos09@users.noreply.github.com>
2023-06-16 10:26:12 -0300

    teste

--- maratonajava/introducao/EstruturaDeRepeticao.java
+++ maratonajava/introducao/EstruturaDeRepeticao.java
@@ -2,6 +2,7 @@ package academy.devdojo.maratonajava.introducao;
 
 public class EstruturaDeRepeticao {
     public static void main(String[] args) {
+        System.out.println("teste");
 
     }
 }
