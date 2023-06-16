c018c623e865c64066620d1db4fb170c37ba7447
João Vítor Ramos <136603790+JoaoRamos09@users.noreply.github.com>
2023-06-16 15:40:30 -0300

    criação de pagina

---
+++ maratonajava/introducao/Arrays.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.introducao;
+
+public class Arrays {
+    public static void main(String[] args) {
+        int[] idades = new int[3];
+        idades[0] = 1;
+        idades[1] = 2;
+        idades[2] = 3;
+        System.out.println(idades[1]);
+
+        int[] idade = new int[3];
+        idade[0] = 1;
+        idade[1] = 2;
+        idade[2] = 3;
+
+        for (int i = 0; i < idade.length; i++) {
+            System.out.println(idade[i]);
+        }
+    }
+}
--- maratonajava/introducao/EstruturaDeRepeticao.java
+++ maratonajava/introducao/EstruturaDeRepeticao.java
@@ -2,8 +2,66 @@ package academy.devdojo.maratonajava.introducao;
 
 public class EstruturaDeRepeticao {
     public static void main(String[] args) {
-        System.out.println("teste");
-        System.out.println("teste");
+        // while
+        int count = 0;
+        while (count < 10) {
+            count++;
+            System.out.println(count);
+        }
+        count = 0;
 
+        // do while
+        do {
+            count++;
+            System.out.println("Dentro do do while " + count);
+        } while (count < 10);
+
+        // for
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println("for " + i);
+        }
+
+        for (int j = 0; j <= 1000000; j++) {
+            if (j % 2 == 0) {
+                System.out.println(j);
+            }
+        }
+
+        // break
+
+        for (int a = 0; a < 50; a++) {
+            System.out.println(a);
+            if (a == 25) {
+                break;
+            }
+        }
+
+        int valorCarro = 40000;
+        int valorMaxParcela = 1000;
+
+       for (int i = 1; i <= valorCarro; i++) {
+            double valorParcela = valorCarro / i;
+            if (valorParcela < valorMaxParcela) {
+                break;
+            }
+            System.out.println("O valor da parcela é de " + valorParcela + " em " + i + "X");
+        }
+        System.out.println("--------------------------------");
+        // continue
+
+        int valorCarro1 = 40000;
+        int valorMaxParcela1 = 1000;
+
+        for (int o = valorCarro1; o >=1; o--) {
+            double valorParcela1 = valorCarro1/o;
+            if(valorParcela1<1000){
+                continue;
+            }
+            System.out.println("O valor da parcela é de "+ valorParcela1+ " em "+ o + "X");
+
+        }
     }
 }
+
+
